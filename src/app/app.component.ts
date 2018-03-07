06c36880e2864a7c85a5c31dd04db59e50d46945
Yaash21 <yaashjain21212@gmail.com>
2018-03-07 17:13:07 +0530

    Test1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'test1';
+  submit(event,value){
+    console.log(value);
+    console.log(event);
+}
 }
